c497e9de348cea2dbf2919c2df6815fb79d8bc93
Zakaria Ali <najkacade100@gmail.com>
2022-06-10 19:36:07 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+const testVar = "Wow"
+
+/* Enter the code to remove the node element here */
+document.querySelector("main#main").remove();
+
+/* Create your new element here and assign it to newHeader */
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Zakaria is the champion!";
+newHeader.className = "victory";
